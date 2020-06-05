24c8d3eff06624c2a6ccf8ef50bdeaf0986d2600
DARTAY-DN <66309262+DARTAY-DN@users.noreply.github.com>
2020-06-05 15:20:15 +0600

    Update mines.py

--- mines.py
+++ mines.py
@@ -13,6 +13,14 @@ while score_not:
 	[ 7 ] [ 8 ] [ 9 ]''')
 	try:
 		result = int(input('-> '))
+		if result >= 9:
+			print('Шансы только от 1 до 9!')
+			score_not = False
+			score -= 9999999
+		if result <= 1:
+			print('Шансы только от 1 до 9!')
+			score_not = False
+			score -= 9999999
 		resone = random.choice(['Верно', 'Неверно'])
 		if resone == 'Верно':
 			print('You Win')
@@ -31,5 +39,11 @@ while score_not:
 			if score <= -1:
 				score_not = False
 				print('У тебя не осталось очков,Перезапусти игру!')
+		if score == 12:
+			name = input('Ты Победил, как тебя звать победитель? ')
+			time.sleep(1)
+			print('Спасибо за игру, ' +  str(name))
+			score_not = False
+	
 	except ValueError:
 		print('Нужно вводить числа!!!')
